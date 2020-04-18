d0f5cab98d56273f65a266dc53bef1f43e412b54
sriramravi7 <sriram.ravirv@gmail.com>
2020-04-18 17:36:05 +0530

    koko

--- src/binb/com/org/BINB1.java
+++ src/binb/com/org/BINB1.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("first");
 	System.out.println("second");
 	j=i+k;
+	System.setProperty("webdriver.chrome.driver", "");
 	System.out.println("nwe1");
 	System.out.println("new2");
 	System.out.println("third");
