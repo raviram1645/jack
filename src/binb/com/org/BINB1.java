b7266f62d4dd24185731832b62ed5ef4013b6643
sriramravi7 <sriram.ravirv@gmail.com>
2020-04-18 17:15:46 +0530

    hoasdf

--- src/binb/com/org/BINB1.java
+++ src/binb/com/org/BINB1.java
@@ -4,6 +4,7 @@ public class BINB1 {
 public static void main(String[] args) {
 	System.out.println("first");
 	System.out.println("second");
+	j=i+k;
 	System.out.println("nwe1");
 	System.out.println("new2");
 	System.out.println("third");
