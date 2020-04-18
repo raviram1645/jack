adfc6058dfcd29320ea6505b8002c6b77bdb9174
sriramravi7 <sriram.ravirv@gmail.com>
2020-04-18 16:34:51 +0530

    first

---
+++ src/binb/com/org/BINB1.java
@@ -0,0 +1,10 @@
+package binb.com.org;
+
+public class BINB1 {
+public static void main(String[] args) {
+	System.out.println("first");
+	System.out.println("second");
+	System.out.println("third");
+	System.out.println("fourth");
+}
+}
